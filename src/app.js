c980dd63775ab297b04d147920ec552c3a4ed5ed
AnnaS30 <windywind@bigmir.net>
2022-10-08 20:52:29 +0300

    updated the time and date

--- src/app.js
+++ src/app.js
@@ -1,18 +1,43 @@
 let apiKey = "9125495e17811cb9a249d8d9e6c6b3f7";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=${apiKey}&units=metric`;
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hour}:${minutes}`;
+}
+
 function showTemp(response) {
   let tempElement = document.querySelector("#temperature");
   let describtionElement = document.querySelector("#describtion");
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
   tempElement.innerHTML = Math.round(response.data.main.temp);
   describtionElement.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 axios.get(apiUrl).then(showTemp);
